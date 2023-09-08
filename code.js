5e1b48ec781136db3ddcc3018779a8576953dbc5
OscarM2001 <mjoelcedeo@gmail.com>
2023-09-08 01:13:40 -0500

    juego de piedra papel o tijeras por consola funcional

---
+++ code.js
@@ -0,0 +1,46 @@
+
+function getComputerChoice(){
+    let answerComputer= Math.ceil(Math.random()*3);
+    if (answerComputer == 1) {
+        return 'rock';
+    }
+    else if (answerComputer ==2)  {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
+}
+
+
+function round(playerSelection, computerSelection){
+    
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'You Lose! Paper beats Rock';
+    } 
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'You Lose! Scissors beats paper';
+    } 
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'You Lose! Rock beats scissors';
+    } 
+    else if (playerSelection == computerSelection) {
+        return 'tied round';
+    }
+    else {
+        return 'You Win! ' + playerSelection + ' beats ' +computerSelection;
+    }
+}
+
+
+function game(){
+    
+    for (let i  = 1; i<=5 ; i++) {
+        let playerSelection = prompt("Enter your rock paper scissors play");
+        const computerSelection = getComputerChoice();
+        let playRound = round(playerSelection,computerSelection);
+        console.log(playRound);
+    }
+}
+
+game();
